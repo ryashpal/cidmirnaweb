067e9934bdd357b2826cffecebdf9c5208b5474c
Yashpal Ramakrishnaiah <yram0006@student.monash.edu>
2020-11-30 14:34:18 +1100

    reloading generated results

---
+++ linc2function/linc2functionUsecase.py
@@ -0,0 +1,56 @@
+import os
+import logging
+import uuid
+from django.conf import settings
+from Bio import SeqIO
+
+from .calculateCodingPotentialUsecase import calculate as calculateCodingPotential
+from .predictSecondaryStructureUsecase import predict as predictSecondaryStructure
+from .predictBindingSitesUsecase import predict as predictBindingSites
+
+
+def annotateFastaString(fasta, model):
+    uid = str(uuid.uuid4())
+    outputPath = os.path.join(settings.BASE_DIR, 'static', 'tmp', uid)
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
+    fastaFilePath = os.path.join(outputPath, uid + '.fasta')
+    with open(fastaFilePath, 'w') as fastaFile:
+        fastaFile.write(fasta)
+    return annotateFastaFile(uid, model)
+
+
+def annotateFastaFile(uid, model):
+    outputPath = os.path.join(settings.BASE_DIR, 'static', 'tmp', uid)
+    fastaFilePath = os.path.join(outputPath, uid + '.fasta')
+    sequence = ''
+    fasta_id = ''
+    for record in SeqIO.parse(fastaFilePath, "fasta"):
+        if record:
+            sequence = str(record.seq)
+            fasta_id = record.id
+            break
+
+    args = {}
+    if sequence and fasta_id:
+        percentage = calculateCodingPotential(sequence, model)
+        radiateImageName, lineImageName = predictSecondaryStructure(fasta_id, uid)
+        arc_diagram_path = os.path.join('tmp', uid, radiateImageName)
+        twod_diagram_path = os.path.join('tmp', uid, lineImageName)
+        headers, data = predictBindingSites(sequence)
+        url = settings.EXTERNAL_BASE_URL + '/linc2function?uid=' + uid + '&model=' + model
+        args = {
+            'percentage': percentage, 
+            'sequence': sequence, 
+            'model': model, 
+            'arc_diagram_path': arc_diagram_path, 
+            'twod_diagram_path': twod_diagram_path, 
+            'headers': headers, 
+            'data': data, 
+            'transcript_id': fasta_id, 
+            'url': url, 
+            }
+        logging.info('linc2function|' + model + '|' + fasta_id + '|' + sequence + '|' + uid + '|' + str(percentage))
+
+    return args
+
--- linc2function/views.py
+++ linc2function/views.py
@@ -1,57 +1,30 @@
-import os
-import random
-import logging
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import render
-from Bio import SeqIO
-from tempfile import NamedTemporaryFile
-
-
-from .calculateCodingPotentialUsecase import calculate as calculateCodingPotential
-from .predictSecondaryStructureUsecase import predict as predictSecondaryStructure
-from .predictBindingSitesUsecase import predict as predictBindingSites
 from .forms import Linc2functionForm
+from .linc2functionUsecase import annotateFastaString as annotateFastaString
+from .linc2functionUsecase import annotateFastaFile as annotateFastaFile
 # Create your views here.
 
+import logging
+
 def linc2function(request):
+    logging.error('linc2function: beginning of the function')
     if request.method == 'POST':
         form = Linc2functionForm(request.POST)
         if form.is_valid():
             fasta = form.cleaned_data['fasta']
-            fastaFile = NamedTemporaryFile(delete=False, suffix='.fasta')
-            fastaFile.write(fasta.encode())
-            fastaFile.close()
-
-            sequence = ''
-            fasta_id = ''
-            for record in SeqIO.parse(fastaFile.name, "fasta"):
-                if record:
-                    sequence = str(record.seq)
-                    fasta_id = record.id
-                    break
-
-            if sequence and fasta_id:
-                model = request.POST['modelRadios']
-                percentage = calculateCodingPotential(sequence, model)
-                uid, radiateImageName, lineImageName = predictSecondaryStructure(fasta_id, fastaFile)
-                arc_diagram_path = os.path.join('tmp', uid, radiateImageName)
-                twod_diagram_path = os.path.join('tmp', uid, lineImageName)
-                headers, data = predictBindingSites(sequence)
-                args = {
-                    'percentage': percentage, 
-                    'sequence': sequence, 
-                    'model': model, 
-                    'arc_diagram_path': arc_diagram_path, 
-                    'twod_diagram_path': twod_diagram_path, 
-                    'headers': headers, 
-                    'data': data, 
-                    'transcript_id': fasta_id, 
-                    }
-                logging.info('linc2function|' + model + '|' + fasta_id + '|' + sequence + '|' + uid + '|' + str(percentage))
-                os.unlink(fastaFile.name)
-                return render(request, 'linc2function_result.html', args)
-            os.unlink(fastaFile.name)
+            model = request.POST['modelRadios']
+            args = annotateFastaString(fasta, model)
+            return render(request, 'linc2function_result.html', args)
+    elif request.method == 'GET':
+        uid = request.GET.get('uid', '')
+        model = request.GET.get('model', '')
+        logging.error('linc2function: ' + uid)
+        logging.error('linc2function: ' + model)
+        if uid and model:
+            args= annotateFastaFile(uid, model)
+            return render(request, 'linc2function_result.html', args)
     form = Linc2functionForm()
     return render(request, 'linc2function.html', {'form': form})
 
